cf8dc3fc340eb85791c32204188fc88dfcbfb29b
goosocial <43911936+goosocial@users.noreply.github.com>
2018-10-08 13:17:53 +0800

    define LANGUAGE ZH

--- wp-config.php
+++ wp-config.php
@@ -38,6 +38,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/** define LANGUAGE */
+define ('WPLANG', 'zh_TW');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
